a7500bb7bacbbbf9715887aab0817c01b41426f7
Pranjal Arote <131807463+Pranjal-Arote@users.noreply.github.com>
2024-03-19 14:33:00 +0530

    Add files via upload

---
+++ Solarpower.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 23:10:03 2024
+
+@author: Pranjal Arote
+"""
+'''
+Solar power consumption has been recorded by city councils at regular 
+intervals. The reason behind doing so is to understand how businesses are 
+using solar power so that they can cut down on nonrenewable sources of energy 
+and shift towards renewable energy. Based on the data, build a forecasting 
+model and provide insights on it. 
+'''
+
+
